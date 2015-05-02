2cbfa9ae9d1b655375140ae2202140277579d382
sniperx21x <timmy_machine@mbpdetiymachine.home>
2015-05-02 11:26:10 +0200

    new

--- src/main/scala/Actor/IRCheckingActor.scala
+++ src/main/scala/Actor/IRCheckingActor.scala
@@ -40,8 +40,8 @@ class IRCheckingActor extends Actor {
           val nbrPlaceAvailableF1 = nbrPlaceAvailable(places, "f1")
           val nbrPlaceAvailableTot = nbrPlaceAvailableF0 + nbrPlaceAvailableF1
           UtilConsole.showMessage("Places du parking disponibles : " + nbrPlaceAvailableTot, getClass.getName ,"INFORMATION_MESSAGE")
-          UtilConsole.showMessage("Etage 0 : Places du parking disponibles : " + nbrPlaceAvailableF0, getClass.getName ,"INFORMATION_MESSAGE")
-          UtilConsole.showMessage("1er étage : Places du parking disponibles : " + nbrPlaceAvailableF1, getClass.getName ,"INFORMATION_MESSAGE")
+          UtilConsole.showMessage("Etage 0 : Places disponibles : " + nbrPlaceAvailableF0, getClass.getName ,"INFORMATION_MESSAGE")
+          UtilConsole.showMessage("Etage 1 : Places disponibles : " + nbrPlaceAvailableF1, getClass.getName ,"INFORMATION_MESSAGE")
           if (nbrPlaceAvailableTot == 0)
             Config.IK.eteindre_led(MyProperties.LED_TOT)
           else
@@ -49,11 +49,11 @@ class IRCheckingActor extends Actor {
           if (nbrPlaceAvailableF0 == 0)
             Config.IK.eteindre_led(MyProperties.LED_F0)
           else
-            Config.IK.allumer_led(MyProperties.LED_TOT)
+            Config.IK.allumer_led(MyProperties.LED_F0)
           if (nbrPlaceAvailableF1 == 0)
             Config.IK.eteindre_led(MyProperties.LED_F1)
           else
-            Config.IK.allumer_led(MyProperties.LED_TOT)
+            Config.IK.allumer_led(MyProperties.LED_F1)
         }
         case Failure(exc) => UtilConsole.showMessage("Error with webservice", getClass.getName, "ERROR_MESSAGE")
       }
